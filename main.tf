c717b4e001903f5bf6777cc1c52445ed6426585a
Justin Christian <justin.christian@gdn-commerce.com>
2024-07-15 18:27:44 +0700

    terraform

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = "1.9.2"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.58.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "ap-southeast-1"
+}
+
+
+resource "aws_instance" "example" {
+    ami = "ami-060e277c0d4cce553"
+    instance_type = "t2.micro"
+}
